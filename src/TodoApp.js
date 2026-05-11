0f504cf0cded7c030771f0731d3675f60aece1e7
Edrey Pomarez <65086931+epomarez@users.noreply.github.com>
2026-05-11 12:36:26 -0600

    feat: Add some basic methods to TodoApp

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -3,8 +3,16 @@ import { TodoItem } from "./TodoItem";
 import { CheckListItem } from "./CheckListItem";
 
 export const TodoApp = {
-
-    addProject() { },
-    getAllProjects() { },
-    deleteProject() { },
+    Projects: [],
+    addProject(project) {
+        this.Projects.push(project);
+    },
+    getAllProjects() {
+        return this.Projects;
+    },
+    removeProject(projectId) {
+        this.Projects = this.Projects.filter(
+            project => project.id !== projectId
+        );
+    },
 };
