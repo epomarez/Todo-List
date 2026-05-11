f9b33b9a3c91c8d57d7a010c72c362f16927f237
Edrey Pomarez <65086931+epomarez@users.noreply.github.com>
2026-05-11 12:20:50 -0600

    Add main app manager object

---
+++ src/TodoApp.js
@@ -0,0 +1,10 @@
+import { ProjectItem } from "./ProjectItem";
+import { TodoItem } from "./TodoItem";
+import { CheckListItem } from "./CheckListItem";
+
+export const TodoApp = {
+
+    addProject() { },
+    getAllProjects() { },
+    deleteProject() { },
+};
