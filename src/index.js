640c2f6167a051ebb211cc4ea5603a1475e2ece8
Edrey Pomarez <65086931+epomarez@users.noreply.github.com>
2026-05-24 02:38:44 -0600

    feat: Add functionality to add project button

--- src/index.js
+++ src/index.js
@@ -11,6 +11,13 @@ const defaultProject = {
 }
 
 function addProjectElements(projects) {
+    // The while loop purpose is to avoid duplication of 
+    // dom elements
+    while (projectItemsContainer.firstChild) {
+        projectItemsContainer.removeChild(projectItemsContainer.firstChild);
+    }
+
+    // Create an html element according to each stored project
     projects.forEach(project => {
         const projectElement = document.createElement('h2');
         projectElement.setAttribute("class", "projectElement");
@@ -20,6 +27,7 @@ function addProjectElements(projects) {
 
 }
 
+// This code runs every time the app is opened.
 function loadApp(isThereData) {
     if (!isThereData) {
         TodoApp.addProject(new ProjectItem(defaultProject));
@@ -27,5 +35,23 @@ function loadApp(isThereData) {
     }
 }
 
+// This code add events to the buttons
+
+const addProjectBtn = document.getElementById("addProject");
+
+addProjectBtn.addEventListener('click', () => {
+    const projectTitle = prompt("Add the project's title");
+    const projectDescription = prompt("Add the project's description");
+
+    const projectData = {
+        title: projectTitle,
+        description: projectDescription,
+    };
+
+    TodoApp.addProject(projectData);
+    addProjectElements(TodoApp.getAllProjects());
+});
+
+//This is just for testing functionality
 loadApp(false);
 console.log(TodoApp.getAllProjects());
